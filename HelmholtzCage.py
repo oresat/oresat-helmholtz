d984375be21705a59c5a32cbaf05a2d1eb72b4c7
Cory <cory27@pdx.edu>
2019-02-02 20:41:06 -0800

    Cleaning redundant code into functions and housekeeping

--- HelmholtzCage.py
+++ HelmholtzCage.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
+#Power Supply imports
+import serial
+import time
+# Temperature & Magnotometer sensor imports
+import smbus
+
+# Streamline data conversion processes for magnetometer and temperature
+def checksize(measurement, maxval, offset):
+	if measurement > maxval :
+		return measurement - offset
+	else:
+		return measurement
+
+# Convert from beautiful Celsius to terrible Fahrenheit
+def fahr(cels):
+	return cels * 1.8 + 32
+
 def magnotometer():
-    #import smbus
-    #import time
-            
     # Get I2C bus
     bus = smbus.SMBus(1)
             
@@ -21,21 +35,15 @@ def magnotometer():
             
     # Convert the data
     xMag = data[0] * 256 + data[1]
-    if xMag > 32767 :
-            xMag -= 65536
+    xMag = checksize(xMag, 32767, 65536)
             
     yMag = data[2] * 256 + data[3]
-    if yMag > 32767 :
-            yMag -= 65536
+    yMag = checksize(yMag, 32767, 65536)
 
     zMag = data[4] * 256 + data[5]
-    if zMag > 32767 :
-            zMag -= 65536
+    zMag = checksize(zMag, 32767, 65536)
 
-    # Output data to screen
-    print "Magnetic field in X-Axis : %d" %xMag
-    print "Magnetic field in Y-Axis : %d" %yMag
-    print "Magnetic field in Z-Axis : %d" %zMag
+    return xMag, yMag, zMag
     
 ############################################################################
 
@@ -48,9 +56,6 @@ def temperature():
     # MCP9808
     # This code is designed to work with the MCP9808_I2CS I2C Mini Module available from ControlEverything.com.
     # https://www.controleverything.com/content/Temperature?sku=MCP9808_I2CS#tabs-0-product_tabset-2
-
-    #import smbus
-    #import time
 
     # Get I2C bus
     bus = smbus.SMBus(1)
@@ -73,16 +78,9 @@ def temperature():
     data = bus.read_i2c_block_data(0x18, 0x05, 2)
 
     # Convert the data to 13-bits
-    ctemp = ((data[0] & 0x1F) * 256) + data[1]
-    if ctemp > 4095 :
-            ctemp -= 8192
-    ctemp = ctemp * 0.0625
-    ftemp = ctemp * 1.8 + 32
-
-    # Output data to screen
-    print "Sensor 1"
-    print "Temperature in Celsius is    : %.2f C" %ctemp
-    print "Temperature in Fahrenheit is : %.2f F" %ftemp
+    ctemp1 = ((data[0] & 0x1F) * 256) + data[1]
+    ctemp1 = checksize(ctemp1, 4095, 8192)
+    ctemp1 = ctemp1 * 0.0625
 
     #------------------------------------
     bus.write_i2c_block_data(0x1c, 0x01, config)
@@ -92,110 +90,72 @@ def temperature():
 
     data = bus.read_i2c_block_data(0x1c, 0x05, 2)
 
-    ctemp = ((data[0] & 0x1F) * 256) + data[1]
-    if ctemp > 4095 :
-            ctemp -= 8192
-    ctemp = ctemp * 0.0625
-    ftemp = ctemp * 1.8 + 32
+    ctemp2 = ((data[0] & 0x1F) * 256) + data[1]
+    ctemp2 = checksize(ctemp2, 4095, 8192)
+    ctemp2 = ctemp2 * 0.0625
 
-    # Output data to screen
-    print "Sensor 2"
-    print "Temperature in Celsius is    : %.2f C" %ctemp
-    print "Temperature in Fahrenheit is : %.2f F" %ftemp
+    return ctemp1, ctemp2
 
 ############################################################################
 
 #function to set voltage. 1st parameter is voltage, 2nd is PSU #
 def setVolts(voltage, psu):
-        
+        setting = "Asu" + str(voltage * 100) + "\n"
         time.sleep(.1)
         if(psu == 1):
-                ser1.write("Asu" + str(voltage * 100) + "\n")
+                ser1.write(setting)
         elif(psu == 2):
-                ser2.write("Asu" + str(voltage * 100) + "\n")
+                ser2.write(setting)
         elif(psu == 3): 
-                ser3.write("Asu" + str(voltage * 100) + "\n")
+                ser3.write(setting)
 
 #function to set amps. 1st parameter is amps, 2nd is PSU #
 def setAmps(amps, psu):
-        
+        setting = "Asi" + str(amps * 1000) + "\n"
         time.sleep(.1)
         if(psu == 1):
-                ser1.write("Asi" + str(amps * 1000) + "\n")
+                ser1.write(setting)
         elif(psu == 2):
-                ser2.write("Asi" + str(amps * 1000) + "\n")
+                ser2.write(setting)
         elif(psu == 3): 
-                ser3.write("Asi" + str(amps * 1000) + "\n")
+                ser3.write(setting)
 
 #function to turn on/off PSU. 1st param: 1 = on, 0 = off, 2nd param: PSU #
 def setOutput(onoff, psu):
-        
+        setting = "Aso" + str(onoff) + "\n"
         time.sleep(.1)
         if(psu == 1):
-                ser1.write("Aso" + str(onoff) + "\n")
+                ser1.write(setting)
         elif(psu == 2):
-                ser2.write("Aso" + str(onoff) + "\n")
+                ser2.write(setting)
         elif(psu == 3): 
-                ser3.write("Aso" + str(onoff) + "\n")
+                ser3.write(setting)
         
-def turnAllOff():
-        
-        time.sleep(.1)
-        ser1.write("Aso0\n")
-        time.sleep(.1)
-        ser2.write("Aso0\n")
-        time.sleep(.1)
-        ser3.write("Aso0\n")
-         
-                
-def turnAllOn():
-        
-        time.sleep(.1)
-        ser1.write("Aso1\n")
-        time.sleep(.1)
-        ser2.write("Aso1\n")
-        time.sleep(.1)
-        ser3.write("Aso1\n")
+# function to switch all PSUs on or off        
+def turnAllOnOrOff(switch):
+        setOutput(switch, 1)
+        setOutput(switch, 2)
+        setOutput(switch, 3)
 
-#Power Supply imports
-import serial
-import time
-# Temperature & Magnotometer sensor imports
-import smbus
+# function to streamline serial port initialization
+# i'm pretty sure this will work just fine and cut down redundancy
+# but not positive so I'd test it out - Cory
+def serialInit(initport):
+	return serial.Serial(
+		port=initport,
+		baudrate=9600,
+		parity=serial.PARITY_NONE,
+		stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS,
+		timeout=1)
 
 #Initialize serial ports
-ser1 = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
-
-print(ser1.name)  
-
-ser2 = serial.Serial(
-    port='/dev/ttyUSB1',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
-
-print(ser2.name)  
-
-ser3 = serial.Serial(
-    port='/dev/ttyUSB2',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
-
-print(ser3.name)  
+ser1 = serialInit('/dev/ttyUSB0')
+print ser1.name
+ser2 = serialInit('/dev/ttyUSB1')
+print ser2.name
+ser3 = serialInit('/dev/ttyUSB2')
+print ser3.name 
 
 control = 1
 
@@ -227,20 +187,31 @@ while (control != 0):
                         print("\nAmperage set to %s on PSU %d." % (a1, p1))
 
                 elif control == 3:
-                        turnAllOn()
+                        turnAllOnOrOff(1)
                         print("Powering On...")
 
                 elif control == 4:
-                        turnAllOff()
+                        turnAllOnOrOff(0)
                         print("Powering Off...")
 
                 elif control == 5:
                         print("Checking temperatures...")
-                        temperature()
+                        ctemp1, ctemp2 = temperature()
+                        # Output data to screen
+						print "Sensor 1"
+						print "Temperature in Celsius is    : %.2f C" % ctemp1
+						print "Temperature in Fahrenheit is : %.2f F" % fahr(ctemp1)
+						print "Sensor 2"
+						print "Temperature in Celsius is    : %.2f C" % ctemp2
+						print "Temperature in Fahrenheit is : %.2f F" % fahr(ctemp2)
 
                 elif control == 6:
                         print("Checking magnotometer")
-                        magnotometer()
+                        xMag, yMag, zMag = magnotometer()
+                        # Output data to screen
+						print "Magnetic field in X-Axis : %d" % xMag
+						print "Magnetic field in Y-Axis : %d" % yMag
+						print "Magnetic field in Z-Axis : %d" % zMag
 
         except ValueError:
                 print("\n***Invalid Entry***")
