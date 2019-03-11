ae17d9eba55e8da0c42d4fe2adeee75e1ca7653d
Cory <cory27@pdx.edu>
2019-03-10 17:13:54 -0700

    cleaning guts. still needs user input but now gives required currents for each power supply based on a desired uniform magnetic field and the measured environmental magnetic fields in each direction

--- MagneticFieldCurrentRelation.py
+++ MagneticFieldCurrentRelation.py
@@ -2,39 +2,56 @@
 
 pi = 3.14159 # approximation of a mysterious number
 mu = 4 * pi * 10**(-7) # magnetic field constant Tm/A
-halfofsidelength = 0.3302 # meters
-distbetweensides = 0.4572 # meters
-wireturns = 58
-constmultiplier = wireturns * mu / pi
+halfofsidelength = 0.3302 # meters, physical parameter
+distbetweensides = 0.4572 # meters, physical parameter
+wireturns = 58 # physical parameter
+
+constmultiplier = wireturns * mu / pi # pulling constants into a single variable
 
 def distFromOriginSquared(d):
-	return (distbetweensides/2 + d)**2 # meter^2
+    return (distbetweensides/2 + d)**2 # meter^2
 
 def dropOffFromSide(d):
-	A = halfofsidelength ** 2
-	offset = distFromOriginSquared(d)
-	return (2 * A) / ((A + offset) * (2 * A + offset)**0.5) # meter^-1
+    A = halfofsidelength ** 2
+    offset = distFromOriginSquared(d)
+    return (2 * A) / ((A + offset) * (2 * A + offset)**0.5) # meter^-1
 
-def paramMultiplier(d):
-	return dropOffFromSide(d) + dropOffFromSide(-d) # meters^(-1)
-
-def axialMagField(current, d): #get amps return microteslas
-	#get actual current for axis from PSU somehow
-	return current * constmultiplier * paramMultiplier(d) *10**(6) # Teslas
-
-# would be cool to have a function of coords get all three mag fields
-# as the magnitude of a single composite
+def paramMultiplier(d): # parameter is distance from origin, we usually only care about d=0
+    return (dropOffFromSide(d) + dropOffFromSide(-d)) * constmultiplier * 10**(6) # microT/A
 
 def getNeededCurrent(desiredfield): #get microteslas return amps
-	return (desiredfield*10**(-6)) / (paramMultiplier(0) * constmultiplier)
+    return desiredfield / paramMultiplier(0)
+
+def axialMagField(current, d): #get amps return microteslas
+    #need to get actual current for axis from program or PSU somehow
+    return current * paramMultiplier(d)
 
 def interface():
-	while True:
-		desiredfield = raw_input("What is the ideal strength of a magnetic field? (microTeslas)\n")
-		neededcurrent = getNeededCurrent(float(desiredfield))
-		print "Required current from PSU: " + str(neededcurrent) + " amps.\n"
-		wantcurrent = raw_input("What is the current from the power supply? (amps)\n")
-		resultfield = axialMagField(float(wantcurrent), 0)
-		print "Resultant magnetic field: " + str(resultfield) + " microteslas.\n"
+    while True:
+        desiredfield = float(raw_input("What is the ideal strength of the uniform magnetic field? (microTeslas)\n"))
+        print "What is the initial strength of Earth's magnetic field? (microTeslas) (x, y, z)"
+        #in the future these initial values should come from magnetometer
+        efx = float(raw_input("x: "))
+        efy = float(raw_input("y: "))
+        efz = float(raw_input("z: "))
+        
+        #compensation
+        diffx = desiredfield - efx
+        diffy = desiredfield - efy
+        diffz = desiredfield - efz
+        
+        #negative amps means amps with a 180 degree polarity from normal
+        neededcurrentx = getNeededCurrent(diffx)
+        neededcurrenty = getNeededCurrent(diffy)
+        neededcurrentz = getNeededCurrent(diffz)
+        
+        print "Required current from PSU 1: " + str(neededcurrentx) + " amps.\n"
+        print "Required current from PSU 2: " + str(neededcurrenty) + " amps.\n"
+        print "Required current from PSU 3: " + str(neededcurrentz) + " amps.\n"
+        
+        #this block was just for testing equation in the other direction
+        #wantcurrent = raw_input("What is the current from the power supply? (amps)\n")
+        #resultfield = axialMagField(float(wantcurrent), 0)
+        #print "Resultant magnetic field: " + str(resultfield) + " microteslas.\n"
 
 interface()
