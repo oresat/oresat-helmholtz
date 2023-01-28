cacb7ca4e92868c40b43e84b37489f195a912df4
dmitri-mcguckin <dmitri@duck.com>
2023-01-27 21:21:36 -0700

    Add Github Workflows

---
+++ tests/spec_todo.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+class TestTodo(TestCase):
+    """
+    Template test cases for TODO
+    """
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_todo(self):
+        self.assertTrue(True)
