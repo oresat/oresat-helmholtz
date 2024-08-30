b30c92689f04cf924d95ede0a5d0feaa407a3ff4
dkm-ee <dmonahan@pdx.edu>
2024-08-30 14:06:04 -0700

    Updated utils functions\n Added 'set_field_vector()'

--- helmholtzcage/utils.py
+++ helmholtzcage/utils.py
@@ -6,17 +6,20 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
 from .Magnetometer import Magnetometer, MagnetometerCommands
+from .Arduino import Arduino, ArduinoCommands
+from .ZXY6005s import ZXY6005s, ZXY6005sCommands
 
 class Utilities:
     #Class utilities constructor.
-    def __init__(self, meter: Magnetometer):
+    def __init__(self, meter: Magnetometer, arduino: Arduino, psu: ZXY6005s):
         super().__init__()
         self.meter = meter
+        self.arduino = arduino
+        self.psu = psu
         pass
     
     ##Prototype function to calculate the milligauss averages of all 3 axis using the STREAM function. (WIP)
-    def reading_avg(self):
-        
+    def reading_avg(self): 
         #Variables needed. 
         sum_x = 0
         sum_y = 0
@@ -34,34 +37,28 @@ class Utilities:
                 print("sum_y:", sum_y)
                 sum_z += chunk[3]['value']
                 print("sum_z:", sum_z)
-                #count += 1
+                count += 1
             else:
                 print("Warning: bad data encountered.")
         
         #Now find the averages of all 3 axes.
-        x_avg = sum_x/num_iterations 
-        y_avg = sum_y/num_iterations
-        z_avg = sum_z/num_iterations
-
+        x_avg = sum_x/count
+        y_avg = sum_y/count
+        z_avg = sum_z/count
 
         print("x avg:", x_avg)
         print("y avg:", y_avg)
-        print("z avg:", z_avg)           
+        print("z avg:", z_avg) 
         
-        #Negated. 
-        negated_x = 0 - x_avg 
-        negated_y = 0 - y_avg
-        negated_z = 0 - z_avg
-        negate = []
-        negate.append(x_avg, y_avg, z_avg)
-        return negate
+        mag_readings = np.array([x_avg, y_avg, z_avg])
+        return mag_readings
         
     
+    ''' Deprecating
     #Prototype function for getting the currents needed to match to earth's magnetic field. 
     def magnetic_to_current_zero(self):
-        
         #This function to zero out the field in the cage utilizes the negated averages of each cage axis. 
-        
+
         #First, get the y-value (y = desired_mag_field - mag_averages)
         data = self.reading_avg() #Call the reading average function and store the data.
         adjusted_field_x = 0 - data[0]
@@ -75,6 +72,7 @@ class Utilities:
 
         ambient_mag = np.array([adjusted_feild_x, adjusted_field_y, adjusted_field_z])
         return ambient_mag
+    '''
         
     def to_output_current(self, *raw_currents):
        # Maps theoretical current value to an output current to adjust for power supply errors
@@ -84,15 +82,45 @@ class Utilities:
        out_curr_vec = (out_curr_vec + 28.3) // 1.23
        return out_curr_vec
 
-    def to_output_mag(self, raw_mag, ambient_mag):
-       # Adjusts  magnetic field vector for output errors
+    def mag_to_current(self, raw_mag, ambient_mag):
+       # Adjusts magnetic field vector to remove ambient influences and system error
        if len(raw_mag) < 3:
            print("Error: Magnetic field vector must have length 3, got {}".format(len(raw_mag)))
            return np.array([0, 0, 0])
        else:
-           xyz_slope = np.array([-1.27, 1.27, -1.1])
+           xyz_slope = np.array([-1.27, 1.27, -1.1])    # hard-coded slope for linearly approximated output curve
+           ambient_mag = np.array(ambient_mag)          # recorded ambient temperature
            raw_mag = np.array(raw_mag)
-           ambient_mag = np.array(ambient_mag)
            out_mag = (raw_mag - ambient_mag) // xyz_slope
            return out_mag
+    
+    def set_field_vector(self, *target):
+        # Attempts to set magnetic field in cage to the specified vector, assuming zero if argument is left empty
+        target = np.array(target)
+        out_field = np.array([0, 0, 0])
+        out_field += target[:2]           # sets output vector to xyz targets from arguments
+        print("Setting magnetic field to target vector : {}, {}, {}".format(*out_field))
 
+        # calculating current settings
+        ambient_field = self.reading_avg()
+        target_current = self.mag_to_current(out_field, ambient_field)
+        out_current = self.to_output_current(*target_current)
+        
+        if (out_current.min() < -800 or out_current.max() > 800):
+            print("output currents are out of range!!\ncancelling output")
+            return -1
+
+        # updating H-Bridges
+        self.arduino.set_positive_X if out_curr[0] > 0 else self.arduino.set_negative_X
+        self.arduino.set_positive_Y if out_curr[1] > 0 else self.arduino.set_negative_Y
+        self.arduino.set_positive_Z if out_curr[2] > 0 else self.arduino.set_negative_Z
+
+        # updating PSUs
+        for i, dev in enumerate(['X', 'Y', 'Z']):
+            self.psu.set_current_limit(dev, out_current[i])
+
+        # powering up PSUs
+        for dev in ['X', 'Y', 'Z']:
+            self.psu.set_output(dev, 1)
+
+        return 0
