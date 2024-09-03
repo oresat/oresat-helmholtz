11851a36e9196f72810d1a9b65d51ffa3c241fbc
dkm-ee <dmonahan@pdx.edu>
2024-09-03 14:56:34 -0700

    Used vectors in 'to_output_current()' in hopes to improve accuracy to target

--- helmholtzcage/utils.py
+++ helmholtzcage/utils.py
@@ -74,24 +74,26 @@ class Utilities:
         return ambient_mag
     '''
         
-    def to_output_current(self, *raw_currents):
+    def to_output_current(self, target_currents):
        # Maps theoretical current value to an output current to adjust for power supply errors
-       slope = 1.23
-       y_int = 28.3
-       out_curr_vec = np.array(raw_currents)
+       if len(target_currents) < 3:
+           print("Error: Target current provided to 'to_output_current()' must be size (3). Got ({})".format(len(target_currents)))
+       slope = np.array([1.22, 1.23, 1.25])
+       y_int = np.array([26.4, 33.6, 25])
+       out_curr_vec = np.array(target_currents)
        out_curr_vec = (out_curr_vec - y_int) // slope
        return out_curr_vec
 
-    def mag_to_current(self, raw_mag, ambient_mag):
+    def mag_to_current(self, target_mag, ambient_mag):
        # Adjusts magnetic field vector to remove ambient influences and system error
-       if len(raw_mag) < 3:
-           print("Error: Magnetic field vector must have length 3, got {}".format(len(raw_mag)))
+       if len(target_mag) < 3:
+           print("Error: Magnetic field vector must have length 3, got {}".format(len(target_mag)))
            return np.array([0, 0, 0])
        else:
            xyz_slope = np.array([-1.27, 1.27, -1.1])    # hard-coded slope for linearly approximated output curve
            ambient_mag = np.array(ambient_mag)          # recorded ambient temperature
-           raw_mag = np.array(raw_mag)
-           out_mag = (raw_mag - ambient_mag) // xyz_slope
+           target_mag = np.array(target_mag)
+           out_mag = (target_mag - ambient_mag) // xyz_slope
            return out_mag
     
     def set_field_vector(self, target):
@@ -102,7 +104,7 @@ class Utilities:
         # calculating current settings
         ambient_field = self.reading_avg()
         target_current = self.mag_to_current(out_field, ambient_field)
-        out_current = self.to_output_current(*target_current)
+        out_current = self.to_output_current(target_current)
         
         if (out_current.min() < -800 or out_current.max() > 800):
             print("output currents are out of range!!\ncancelling output")
