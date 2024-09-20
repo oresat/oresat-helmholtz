0814be3121292a19b37d6b61643535364871781e
danielkm-ee <dannym1432@gmail.com>
2024-09-20 12:51:42 -0700

    Updated function arguments and their defaults Added constants `MAG_CURRENT_SLOPE` and `WMM_AMBIENT_FIELD` to use as default conversion factors Added constant `MAX_OUT_CURRENT` used to limit the output current in set_field_vector()

--- helmholtzcage/utils.py
+++ helmholtzcage/utils.py
@@ -9,6 +9,10 @@ from .Magnetometer import Magnetometer, MagnetometerCommands
 from .ZXY6005s import ZXY6005s, ZXY6005sCommands
 from .Arduino import Arduino, ArduinoCommands
 
+MAX_OUT_CURRENT = 1000                  # 1 amp limit
+MAG_CURRENT_SLOPE = [-1.27, 1.27, -1.1] # Conversion factors for magnetic field to current relation
+WMM_AMBIENT_FIELD = [194, 520, 479]     # (mG) Ambient field according to World Magnetic Model
+
 class Utilities:
     #Class utilities constructor.
     def __init__(self, meter: Magnetometer, psu:ZXY6005s, arduino:Arduino):
@@ -90,30 +94,36 @@ class Utilities:
        out_curr_vec = (out_curr_vec - y_int) // slope
        return out_curr_vec
 
-    def mag_to_current(self, target_mag, ambient_mag):
-       # Adjusts magnetic field vector to remove ambient influences and system error
+    def mag_to_current(self, target_mag, xyz_slope=MAG_CURRENT_SLOPE, ambient_mag=WMM_AMBIENT_FIELD):
+       # Adjusts magnetic field vector to remove ambient influences and system error using linear regression coefficients
        if len(target_mag) < 3:
            print("Error: Target field vector must have length 3, got {}".format(len(target_mag)))
            return np.array([0, 0, 0])
        else:
-           xyz_slope = np.array([-1.27, 1.27, -1.1])    # hard-coded slope for linearly approximated output curve
-           ambient_mag = np.array(ambient_mag)          # recorded ambient temperature
            target_mag = np.array(target_mag)
+           xyz_slope = np.array(xyz_slope)              # slope of mag-current line
+           ambient_mag = np.array(ambient_mag)          # intercept of mag-current line
+
            out_mag = (target_mag - ambient_mag) // xyz_slope
            return out_mag
     
-    def set_field_vector(self, target):
+    def set_field_vector(self, target=[0, 0, 0], xyz_slope=MAG_CURRENT_SLOPE, ambient_field=WMM_AMBIENT_FIELD):
         # Attempts to set magnetic field in cage to the specified vector, assuming zero if argument is left empty
-        out_field = np.array(target)          # xyz targets from arguments
+        out_field = np.array(target)            # xyz targets from arguments
+        ambient_field = np.array(ambient_field) # average field from earth
+
+        if (out_field.shape != (3,) or ambient_field.shape != (3,)):
+            print("Error: Vectors provided to set_field_vector() must be of shape (3,). Got {} and {}.".format(out_field.shape, ambient_field.shape) )
+            return -1
 
         # calculating current settings
-        ambient_field = self.reading_avg()
-        target_current = self.mag_to_current(out_field, ambient_field)
+        target_current = self.mag_to_current(out_field, xyz_slope, ambient_field)
         out_current = self.to_output_current(target_current)
         
-        if (out_current.min() < -800 or out_current.max() > 800):
+        if (np.abs(out_current).max() > MAX_OUT_CURRENT):
             print("output currents are out of range!!\ncancelling output")
             return -1
+
         print("Setting magnetic field to target vector :", out_field)#{}, {}, {}".format(*out_field))
 
         # updating H-Bridges
