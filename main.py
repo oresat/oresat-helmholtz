efe985c8335399eb30a072824eba3bb547f47afd
William Jeffreys <wjeffreys96@gmail.com>
2026-01-15 00:36:43 -0800

    Set up I2C
    create the i2c driver and give it the addresses to the three ina226s

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from time import sleep
-
 import board
+from busio import I2C
 from digitalio import DigitalInOut, Direction
 
 import adafruit_logging as logging
+from blink import blink_led
+from ina226 import INA226
 from motor import Motor
 
 logger = logging.getLogger('Helmholtz logger')
@@ -14,20 +15,17 @@ logger.info("Oresat Helmholtz Cage Firmware v2")
 PICO_LED = DigitalInOut(board.LED)
 PICO_LED.direction = Direction.OUTPUT
 
+i2c = I2C(board.GP1, board.GP0)
+
+print(dir(i2c))
+
+# ina226_x = INA226(i2c, 0x40)
+# ina226_y = INA226(i2c, 0x41)
+# ina226_z = INA226(i2c, 0x42)
+
 motor_x = Motor(in1=board.GP2, in2=board.GP3, led=board.GP13)
 motor_y = Motor(in1=board.GP6, in2=board.GP7, led=board.GP14)
 motor_z = Motor(in1=board.GP10, in2=board.GP11, led=board.GP15)
 
 while True:
-    PICO_LED.value = True
-    sleep(1)
-    PICO_LED.value = False
-    motor_x.forward()
-    sleep(1)
-    motor_x.stop()
-    motor_y.reverse()
-    sleep(1)
-    motor_y.stop()
-    motor_z.reverse()
-    sleep(1)
-    motor_z.stop()
+    blink_led(PICO_LED, 1)
