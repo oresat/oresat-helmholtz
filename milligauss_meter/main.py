0bc30330db2c6ce3fc7c2b32a8493f66e87b159a
William Jeffreys <wjeffreys96@gmail.com>
2026-01-15 14:18:37 -0800

    Create function to parse data received from the MR3
    parse_stream will take in the byte stream, break it into chunks, and
    then use the constants in constants.py, which are pulled from the
    comm_protocol docs, to make sense of the stream. Looking at the mr3
    while the script is running, everything seems to line up, except that
    the field_type returns 3, which is labeled as just "11=" int the docs,
    which doesnt make much sense. Nonetheless, it's exciting that this
    works. Next step will be to send that data over uart from the rpi5 to
    the pic0.

--- milligauss_meter/main.py
+++ milligauss_meter/main.py
@@ -5,33 +5,38 @@ import serial
 
 
 def parse_stream(stream):
-    if len(stream) == 31:
+    if len(stream) != 31:
         # Sometimes we get incomplete data that we should ignore
         # 5 chunks * 6 bytes each + 1 acknowledge byte = 31 make up a valid stream
         # TODO: Handle incomplete data
         return
 
-    chunks = [stream[i : i + 1] for i in range(0, len(stream), 6)]
+    chunks = [stream[i : i + 6] for i in range(0, len(stream), 6)]
 
     for i, chunk in enumerate(chunks):
+        if i == 5:
+            # Last byte is the acknowledge byte
+            continue
+
         # First two bytes are config data
         config = chunk[:2]
+
         data_null = config[0] & const.NULL_DATA_MSK
-        field_type = config[0] & const.FIELD_TYPE_MSK
+        field_type = (config[0] & const.FIELD_TYPE_MSK) >> 4
         sett_changed = config[0] & const.SETT_CHANGED_MSK
         sign = config[1] & const.SIGN_MSK
         decimal_place = config[1] & const.DECIMAL_PLACE_MSK
 
-        # Last 4 bytes are the data
-        data = chunk[2:]
+        # Last 3 bytes are the data
+        data = int.from_bytes(chunk[2:], byteorder="big") / (10**decimal_place)
 
-        print(f"Chunk {i}:)")
-        print(f"Config: {config}")
+        chunk_types = ["Time", "X", "Y", "Z", "Mag"]
+
+        print(8 * "=" + f" {chunk_types[i]} " + 8 * "=")
         print(f"Data null: {data_null}")
         print(f"Field type: {field_type}")
         print(f"Settings changed: {sett_changed}")
         print(f"Sign: {'+' if sign == 0 else '-'}")
-        print(f"Decimal place: {decimal_place}")
         print(f"Data: {data}")
 
 
@@ -44,7 +49,7 @@ ser.write(const.KILL_ALL_PROCESS_CMD)
 ser.write(const.RESET_TIME_CMD)
 
 while True:
-    # Continuously stream and parse valid data from the MR3
+    # Continuously stream and parse data from the MR3
     stream = ser.read_until(b'\x08')
     parse_stream(stream)
 
