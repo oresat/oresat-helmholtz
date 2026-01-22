ee97fe0d91ec3a36a76c24991729f17aaeb07dec
William Jeffreys <wjeffreys96@gmail.com>
2026-01-22 14:03:54 -0800

    Complete mr3_reader code initial version

--- mr3_reader/mnt/main.py
+++ mr3_reader/mnt/main.py
@@ -2,7 +2,7 @@ import time
 from struct import pack
 
 import constants as const
-import serial
+from serial import Serial, SerialException
 
 from cobsr import encode
 
@@ -62,18 +62,32 @@ def pack_data(data):
     )
 
 
-print("Setting up serial ports at /dev/ttyAMA0 and /dev/ttyUSB0")
-mr3_ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.1)
-pico_ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=5)
+def wait_for_serial(port, timeout=60):
+    time_now = time.time()
 
-print("Resetting MR3")
+    while time.time() - time_now < timeout:
+        try:
+            ser = Serial(port, 115200, timeout=0.1)
+            return ser
+        except SerialException:
+            print(f"Unable to connect to {port}. Trying again", flush=True)
+            time.sleep(1)
+
+    raise SerialException(f"Failed to connect to {port} after {timeout} seconds")
+
+
+print("Setting up serial ports at /dev/ttyAMA0 and /dev/ttyMR3", flush=True)
+mr3_ser = wait_for_serial("/dev/ttyMR3")
+rpi_ser = wait_for_serial("/dev/ttyAMA0")
+
+print("Resetting MR3", flush=True)
 mr3_ser.write(const.KILL_ALL_PROCESS_CMD)  # Tell the MR3 to stop whatever it's doing
 mr3_ser.write(
     const.RESET_TIME_CMD
 )  # Start the session by resetting the time per prototocol
 
 
-print("Sending field measurements to Pico")
+print("Sending magnetic field measurements to Pico", flush=True)
 try:
     while True:
         stream = mr3_ser.read_until(b"\x08")  # Read data from the MR3
@@ -85,9 +99,9 @@ try:
             if data is not None:
                 packet = pack_data(data)
                 if b"\x00" in packet[1:]:
-                    print("WARNING: 0 found in packet contents")
+                    print("WARNING: 0 found in packet contents", flush=True)
                 else:
-                    pico_ser.write(packet)
+                    rpi_ser.write(packet)
 
         time.sleep(0.5)  # The mr3 sends data at a rate of 2hz
 
@@ -95,4 +109,4 @@ try:
 except KeyboardInterrupt:
     pass
 finally:
-    print("Ending MR3 reader service")
+    print("Ending MR3 reader service", flush=True)
