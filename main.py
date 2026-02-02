5ac9a548913a3d17e490e4a0116592201ca1a977
William Jeffreys <wjeffreys96@gmail.com>
2026-02-01 16:15:10 -0800

    Refactor to undo arrow anti-pattern

--- main.py
+++ main.py
@@ -2,37 +2,60 @@ import time
 
 import board
 from busio import I2C, UART
+from UART import get_mag_field
 
 import adafruit_logging as logging
 from ina226 import INA226
 from motor import Motor
-from uart import get_mag_field
 
-logger = logging.getLogger('Helmholtz logger')
-logger.setLevel(logging.DEBUG)
+MAG_FIELD_BUF = bytearray()
+MAG_READ_DURATION = 0.5
+LAST_MAG_READ_TIME = -1
+LAST_MOTOR_DRIVE_TIME = -1
+MAG_FIELD = None
 
-logger.info("Oresat Helmholtz Cage Firmware v2")
+LOGGER = logging.getLogger('Helmholtz logger')
+LOGGER.setLevel(logging.DEBUG)
 
-uart = UART(board.GP16, board.GP17, baudrate=115200, timeout=0.01, receiver_buffer_size=256)
-logger.info("Initialized uart: %s", uart)
+LOGGER.info("Oresat Helmholtz Cage Firmware v2")
 
-i2c = I2C(board.GP1, board.GP0)
-logger.info("Initialized i2c: %s", i2c)
+UART = UART(board.GP16, board.GP17, baudrate=115200, timeout=0.01, receiver_buffer_size=64)
+LOGGER.info("Initialized UART")
 
-# ina226_x = INA226(i2c, 0x40)
-# ina226_y = INA226(i2c, 0x41)
-# ina226_z = INA226(i2c, 0x42)
+I2C = I2C(board.GP1, board.GP0)
+LOGGER.info("Initialized I2C bus")
 
-motor_x = Motor(in1=board.GP2, in2=board.GP3, led=board.GP13)
-motor_y = Motor(in1=board.GP6, in2=board.GP7, led=board.GP14)
-motor_z = Motor(in1=board.GP10, in2=board.GP11, led=board.GP15)
-logger.info("Initialized motor drivers")
+INA226_X = INA226(I2C, 0x40)
+INA226_Y = INA226(I2C, 0x41)
+INA226_Z = INA226(I2C, 0x42)
+LOGGER.info("Initialized INA226 drivers")
 
-mag_field_buf = bytearray()
+MOTOR_X = Motor(in1=board.GP2, in2=board.GP3, led=board.GP13)
+MOTOR_Y = Motor(in1=board.GP6, in2=board.GP7, led=board.GP14)
+MOTOR_Z = Motor(in1=board.GP10, in2=board.GP11, led=board.GP15)
+LOGGER.info("Initialized motor drivers")
 
-logger.info("Entering main loop")
+LOGGER.info("Entering main loop")
 while True:
-    mag_field_buf, mag_field = get_mag_field(uart, mag_field_buf, logger)
+    time_now = time.monotonic()
 
-    if mag_field != 0:
-        logger.info(mag_field)
+    if time_now >= 0.25 + LAST_MOTOR_DRIVE_TIME:
+        MOTOR_X.reverse(100)
+        MOTOR_Y.reverse(100)
+        MOTOR_Z.reverse(100)
+        LAST_MOTOR_DRIVE_TIME = time_now
+
+    if time_now >= MAG_READ_DURATION + LAST_MAG_READ_TIME:
+        try:
+            MAG_FIELD_BUF, mag_field = get_mag_field(UART, MAG_FIELD_BUF, LOGGER)
+        except ValueError as e:
+            LOGGER.error(e)
+            continue
+
+        LOGGER.info(["X", "Current:", INA226_X.current, "Bus Voltage:", INA226_X.bus_voltage])
+        LOGGER.info(["Y", "Current:", INA226_Y.current, "Bus Voltage:", INA226_Y.bus_voltage])
+        LOGGER.info(["Z", "Current:", INA226_Z.current, "Bus Voltage:", INA226_Z.bus_voltage])
+        LOGGER.info(mag_field)
+        MAG_FIELD = mag_field
+
+        LAST_MAG_READ_TIME = time_now
--- uart.py
+++ uart.py
@@ -8,31 +8,34 @@ def get_mag_field(uart, buf, logger):
     err = 0
     resp = uart.read(64)
 
-    if resp is not None and len(resp) > 0:
-        PICO_LED.value = True
+    if not resp:
+        raise ValueError("No response from serial bridge")
 
-        buf.extend(resp)
-        delim_pos = buf.find(b'\x00')
+    PICO_LED.value = True
 
-        if delim_pos != -1 and len(buf) >= delim_pos + 21:
-            packet = buf[delim_pos + 1 : delim_pos + 21]
+    buf.extend(resp)
+    delim_pos = buf.find(b'\x00')
 
-            new_buf = buf[delim_pos + 21 :]
+    if delim_pos == -1 or len(buf) < delim_pos + 21:
+        raise ValueError("Could not parse data from serial bridge")
 
-            try:
-                decoded_data = decode(packet)
-            except DecodeError as e:
-                # logger.error(e)
-                return new_buf, err
-            else:
-                try:
-                    time, x, y, z, mag = unpack('fffff', decoded_data)
-                except (RuntimeError, TypeError) as e:
-                    logger.error("Unpack error: %s", e)
-                    return new_buf, err
-                else:
-                    PICO_LED.value = False
-                    return new_buf, {"time": time, "x": x, "y": y, "z": z, "mag": mag}
+    packet = buf[delim_pos + 1 : delim_pos + 21]
+    new_buf = buf[delim_pos + 21 :]
+
+    try:
+        decoded_data = decode(packet)
+    except DecodeError as e:
+        logger.error(e)
+        return new_buf, err
+
+    try:
+        time, x, y, z, mag = unpack('fffff', decoded_data)
+    except (RuntimeError, TypeError) as e:
+        logger.error("Unpack error: %s", e)
+        return new_buf, err
+    else:
+        PICO_LED.value = False
+        return new_buf, {"time": time, "x": x, "y": y, "z": z, "mag": mag}
 
     PICO_LED.value = False
     return buf, err
