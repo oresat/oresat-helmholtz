694f887e979d42c9a99038f4f6a3a17e4b0a33f4
William Jeffreys <wjeffreys96@gmail.com>
2026-02-01 18:03:05 -0800

    Update pico uart driver
    We can get rid of the buffer now that we are certain about what were
    getting from the rpi5. It will always be 21 bytes, and it comes in very
    consistently now.

--- uart.py
+++ uart.py
@@ -4,38 +4,30 @@ from blink import PICO_LED
 from cobsr import DecodeError, decode
 
 
-def get_mag_field(uart, buf, logger):
-    err = 0
-    resp = uart.read(64)
+def get_mag_field(uart):
+    resp = uart.read(21)
 
     if not resp:
         raise ValueError("No response from serial bridge")
 
     PICO_LED.value = True
 
-    buf.extend(resp)
-    delim_pos = buf.find(b'\x00')
+    delim_pos = resp.find(b'\x00')
 
-    if delim_pos == -1 or len(buf) < delim_pos + 21:
+    if delim_pos == -1:
         raise ValueError("Could not parse data from serial bridge")
 
-    packet = buf[delim_pos + 1 : delim_pos + 21]
-    new_buf = buf[delim_pos + 21 :]
+    packet = resp[:delim_pos]
 
     try:
         decoded_data = decode(packet)
     except DecodeError as e:
-        logger.error(e)
-        return new_buf, err
+        raise ValueError from e
 
     try:
         time, x, y, z, mag = unpack('fffff', decoded_data)
     except (RuntimeError, TypeError) as e:
-        logger.error("Unpack error: %s", e)
-        return new_buf, err
-    else:
-        PICO_LED.value = False
-        return new_buf, {"time": time, "x": x, "y": y, "z": z, "mag": mag}
+        raise ValueError from e
 
     PICO_LED.value = False
-    return buf, err
+    return {"time": time, "x": x, "y": y, "z": z, "mag": mag}
