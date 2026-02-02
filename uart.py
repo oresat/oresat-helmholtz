9c8e1745cf8a9399d822750f3124c35b97b54895
William Jeffreys <wjeffreys96@gmail.com>
2026-02-01 20:40:41 -0800

    Reset uart input buffer on error
    Allows uart to recover from errors and prevents infinite loops when
    retrying reading the bridge until success

--- uart.py
+++ uart.py
@@ -8,25 +8,28 @@ def get_mag_field(uart):
     resp = uart.read(21)
 
     if not resp:
+        uart.reset_input_buffer()
         raise ValueError("No response from serial bridge")
 
     PICO_LED.value = True
-
     delim_pos = resp.find(b'\x00')
 
     if delim_pos == -1:
-        raise ValueError("Could not parse data from serial bridge")
+        uart.reset_input_buffer()
+        raise ValueError(f"Could not parse data from serial bridge: {resp}")
 
     packet = resp[:delim_pos]
 
     try:
         decoded_data = decode(packet)
     except DecodeError as e:
+        uart.reset_input_buffer()
         raise ValueError from e
 
     try:
         time, x, y, z, mag = unpack('fffff', decoded_data)
     except (RuntimeError, TypeError) as e:
+        uart.reset_input_buffer()
         raise ValueError from e
 
     PICO_LED.value = False
